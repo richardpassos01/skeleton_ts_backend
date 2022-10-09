e5302f52a895ed9418bbb71595db33dde536b255
richard <richard.h.passos@hotmail.com>
2022-10-09 04:26:36 -0300

    refactor ic

--- src/DependencyInjectionContainer.ts
+++ src/DependencyInjectionContainer.ts
@@ -4,8 +4,8 @@ import UserRepositoryInterface from './domain/user/repositories/UserRepositoryIn
 
 import Database from './infrastructure/database';
 
-import AuthenticationController from '@api/authentication/AuthenticationController';
-import UserController from '@api/user/UserController';
+import AuthenticationController from './api/authentication/AuthenticationController';
+import UserController from './api/user/UserController';
 
 import AuthenticateUser from './application/use_cases/AuthenticateUser';
 import CreateUser from './application/use_cases/CreateUser';
@@ -17,35 +17,31 @@ import UserRepository from './infrastructure/repositories/UserRepository';
 
 const container = new Container();
 
+container.bind<Database>(TYPES.Database).to(Database).inSingletonScope();
+
 container
   .bind<UserRepositoryInterface>(TYPES.UserRepository)
   .to(UserRepository)
   .inSingletonScope();
 
-container.bind<Database>(TYPES.Database).to(Database).inSingletonScope();
-
 container.bind<CreateUser>(TYPES.CreateUser).to(CreateUser).inSingletonScope();
-
+container
+  .bind<UpdateUserPassword>(TYPES.UpdateUserPassword)
+  .to(UpdateUserPassword)
+  .inSingletonScope();
+container
+  .bind<FetchUserByEmail>(TYPES.FetchUserByEmail)
+  .to(FetchUserByEmail)
+  .inSingletonScope();
 container
   .bind<AuthenticateUser>(TYPES.AuthenticateUser)
   .to(AuthenticateUser)
   .inSingletonScope();
 
 container
-  .bind<UpdateUserPassword>(TYPES.UpdateUserPassword)
-  .to(UpdateUserPassword)
-  .inSingletonScope();
-
-container
-  .bind<FetchUserByEmail>(TYPES.FetchUserByEmail)
-  .to(FetchUserByEmail)
-  .inSingletonScope();
-
-container
   .bind<UserController>(TYPES.UserController)
   .to(UserController)
   .inSingletonScope();
-
 container
   .bind<AuthenticationController>(TYPES.AuthenticationController)
   .to(AuthenticationController)
