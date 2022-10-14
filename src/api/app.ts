f84cdcce90b280245ab69187492dc0d9d5cf7f3b
richard <richard.h.passos@hotmail.com>
2022-10-14 00:22:52 -0300

    refactor: add cors

--- src/api/app.ts
+++ src/api/app.ts
@@ -5,6 +5,7 @@ import * as Koa from 'koa';
 import {TYPES} from '@constants/types';
 import container from '@dependencyInjectionContainer';
 import Database from '@infrastructure/database';
+import * as cors from '@koa/cors';
 import errorHandler from '@middleware/errorHandler';
 import * as bodyParser from 'koa-bodyparser';
 import routes from './routes';
@@ -13,6 +14,7 @@ const app = new Koa();
 
 app.use(bodyParser());
 app.use(errorHandler);
+app.use(cors());
 app.use(routes.routes()).use(routes.allowedMethods()).use(routes.middleware());
 
 (() => {
